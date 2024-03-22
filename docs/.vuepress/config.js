99bde269f759e4bcde4dc880f1071cb7b15e44da
tangwenjian <gistwj@163.com>
2024-03-22 10:38:21 +0800

    feat:初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  base:"gis-tang-blog",
+  lang:'zh-CN',
+  title: 'Tang',
+  description: 'Just playing around',
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    navbar:[
+      {
+        text:'首页',
+        link:'/'
+      },
+      {
+        text:'技术文档',
+        children:[]
+      }
+    ]
+  }),
+})
