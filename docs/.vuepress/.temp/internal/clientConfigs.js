3a8f484f6377ac7f1c5425e0beaab54517f09663
tangwenjian <gistwj@163.com>
2024-03-22 13:37:52 +0800

    feat:主题配置

--- docs/.vuepress/.temp/internal/clientConfigs.js
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -1,11 +1,15 @@
-import clientConfig0 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
-import clientConfig1 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
-import clientConfig2 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
-import clientConfig3 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-external-link-icon/lib/client/config.js'
-import clientConfig4 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
-import clientConfig5 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
-import clientConfig6 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
-import clientConfig7 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/theme-default/lib/client/config.js'
+import clientConfig0 from 'E:/gitee/vuepress-starter/docs/.vuepress/.temp/sass-palette/load-hope.js'
+import clientConfig1 from 'E:/gitee/vuepress-starter/docs/.vuepress/.temp/components/config.js'
+import clientConfig2 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import clientConfig3 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-catalog/lib/client/config.js'
+import clientConfig4 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import clientConfig5 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-external-link-icon/lib/client/config.js'
+import clientConfig6 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import clientConfig7 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import clientConfig8 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import clientConfig9 from 'E:/gitee/vuepress-starter/docs/.vuepress/.temp/md-enhance/config.js'
+import clientConfig10 from 'E:/gitee/vuepress-starter/node_modules/@vuepress/plugin-photo-swipe/lib/client/config.js'
+import clientConfig11 from 'E:/gitee/vuepress-starter/docs/.vuepress/.temp/theme-hope/config.js'
 
 export const clientConfigs = [
   clientConfig0,
@@ -16,4 +20,8 @@ export const clientConfigs = [
   clientConfig5,
   clientConfig6,
   clientConfig7,
+  clientConfig8,
+  clientConfig9,
+  clientConfig10,
+  clientConfig11,
 ]
