83e371e3537a676acaa179d791bbf93e2eace4eb
GISTWJ <71766821+GISTWJ@users.noreply.github.com>
2024-04-09 08:21:20 +0000

    Deploying to gh-pages from @ GISTWJ/gistwj.github.io@f1b0c8f090721ce3e65f81eb3a954c4ad1d32fa3 🚀

---
+++ assets/000、WebGL学习博客搭建.html-Bnq5Tacv.js
@@ -0,0 +1,24 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as n,c as t,e as i}from"./app-BjroUTcS.js";const r={},a=i(`<div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>import { viteBundler } from &#39;@vuepress/bundler-vite&#39;
+import { defaultTheme } from &#39;@vuepress/theme-default&#39;
+import { defineUserConfig } from &#39;vuepress&#39;
+
+export default defineUserConfig({
+  lang:&#39;zh-CN&#39;,
+  title: &#39;Tang&#39;,
+  description: &#39;Just playing around&#39;,
+  bundler: viteBundler(),
+  theme: defaultTheme({
+    navbar:[
+      {
+        text:&#39;首页&#39;,
+        link:&#39;/&#39;
+      },
+      {
+        text:&#39;技术文档&#39;,
+        children:[]
+      }
+    ]
+  }),
+})
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),d=[a];function s(l,o){return n(),t("div",null,d)}const u=e(r,[["render",s],["__file","000、WebGL学习博客搭建.html.vue"]]),v=JSON.parse(`{"path":"/studyNotes/WebGL/000%E3%80%81WebGL%E5%AD%A6%E4%B9%A0%E5%8D%9A%E5%AE%A2%E6%90%AD%E5%BB%BA.html","title":"","lang":"zh-CN","frontmatter":{"head":[["meta",{"property":"og:url","content":"https://mister-hope.github.io/studyNotes/WebGL/000%E3%80%81WebGL%E5%AD%A6%E4%B9%A0%E5%8D%9A%E5%AE%A2%E6%90%AD%E5%BB%BA.html"}],["meta",{"property":"og:site_name","content":"歪脖祭司"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-09T07:59:20.000Z"}],["meta",{"property":"article:author","content":"祭司唐"}],["meta",{"property":"article:modified_time","content":"2024-04-09T07:59:20.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-04-09T07:59:20.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"祭司唐\\",\\"url\\":\\"\\"}]}"]]},"headers":[],"git":{"createdTime":1712649560000,"updatedTime":1712649560000,"contributors":[{"name":"tangwenjian","email":"gistwj@163.com","commits":1}]},"readingTime":{"minutes":0.14,"words":42},"filePathRelative":"studyNotes/WebGL/000、WebGL学习博客搭建.md","localizedDate":"2024年4月9日","excerpt":"<div class=\\"language-text\\" data-ext=\\"text\\" data-title=\\"text\\"><pre class=\\"language-text\\"><code>import { viteBundler } from '@vuepress/bundler-vite'\\nimport { defaultTheme } from '@vuepress/theme-default'\\nimport { defineUserConfig } from 'vuepress'\\n\\nexport default defineUserConfig({\\n  lang:'zh-CN',\\n  title: 'Tang',\\n  description: 'Just playing around',\\n  bundler: viteBundler(),\\n  theme: defaultTheme({\\n    navbar:[\\n      {\\n        text:'首页',\\n        link:'/'\\n      },\\n      {\\n        text:'技术文档',\\n        children:[]\\n      }\\n    ]\\n  }),\\n})\\n\\n</code></pre></div>"}`);export{u as comp,v as data};
